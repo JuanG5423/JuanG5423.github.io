2805cf536e0d947cbd22584974bf65b89580c6d1
JuanG5423 <guajardo5423@gmail.com>
2024-10-24 20:24:14 -0500

    Object properties

--- refactor.py
+++ refactor.py
@@ -5,7 +5,7 @@ def remove_filler(filename : str) -> str:
     #rename the file to the first part of the filename
     return filename[:split]
 
-#remove filler on all files in the dataproperties directory
-for filename in os.listdir("docs/dataproperties"):
-    os.rename("docs/dataproperties/" + filename, "docs/dataproperties/" + remove_filler(filename) + ".html")
+#remove filler on all files in the objectproperties directory
+for filename in os.listdir("docs/objectproperties"):
+    os.rename("docs/objectproperties/" + filename, "docs/objectproperties/" + remove_filler(filename) + ".html")
 
