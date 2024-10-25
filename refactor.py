363e371a79fcb004b5642bf4630281dd3a28586a
JuanG5423 <guajardo5423@gmail.com>
2024-10-24 20:12:30 -0500

    Add data properties

--- refactor.py
+++ refactor.py
@@ -1,11 +1,11 @@
 import os
 
 def remove_filler(filename : str) -> str:
-    split = filename.find("_")
+    split = filename.find("___")
     #rename the file to the first part of the filename
     return filename[:split]
 
-#remove filler on all files in the classes directory
-for filename in os.listdir("docs/classes"):
-    os.rename("docs/classes/" + filename, "docs/classes/" + remove_filler(filename) + ".html")
+#remove filler on all files in the dataproperties directory
+for filename in os.listdir("docs/dataproperties"):
+    os.rename("docs/dataproperties/" + filename, "docs/dataproperties/" + remove_filler(filename) + ".html")
 
